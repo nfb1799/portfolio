b5151aecafa71299ca3be8ce4134023567f7f0bb
nfb1799 <nfb1799@rit.edu>
2023-07-30 01:13:35 -0400

    Self image added to home page

--- portfolio/src/views/Home.vue
+++ portfolio/src/views/Home.vue
@@ -1,19 +1,31 @@
 <template>
-  <div>
-    <v-btn
-      style="width: 100%;"
-    >
-      testing
-    </v-btn>
-  </div>
+  <v-sheet
+    id="home-sheet"
+    color="black"
+    style="height: 100$;"
+  >
+    <v-row>
+      <v-col cols="6">
+        <img
+          src="../assets/self.png"
+        >
+      </v-col>
+      <v-col cols="6">
+        <v-card>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, eius? Libero sint officia dicta tempore fuga fugit omnis quo et magni quam, illum ut temporibus doloremque sed. Dolores, rerum voluptas!
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-sheet>
 </template>
 
 <script>
-export default {
-
-}
 </script>
 
 <style>
-
+#home-sheet {
+  height: 100%;
+  background: url('../assets/stars.jpg');
+  background-size: cover;
+}
 </style>
